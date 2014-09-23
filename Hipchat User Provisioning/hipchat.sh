b675702cf5feae943fc54c6230adfb1d815a5233
Ross Derewianko <rderewianko@pingidentity.com>
2014-09-23 15:08:40 -0700

    Fix
    Had an extra trailing slash in the jss resource which was screwing things up.
    
    Signed-off-by: Ross Derewianko <rderewianko@pingidentity.com>

--- Hipchat User Provisioning/hipchat.sh
+++ Hipchat User Provisioning/hipchat.sh
@@ -37,18 +37,18 @@ fi
 ########################################################################
 # Script
 ########################################################################
+
 #find out the url of the JSS!
 jss=`defaults read /Library/Preferences/com.jamfsoftware.jamf.plist jss_url`
-
+echo "jss is $jss"
 #call jss for info regarding the user that's run the script
-response=$(curl "$jss"/JSSResource/users/name/"$username" --user "$apiuser:$apipass")
+response=$(curl "$jss"JSSResource/users/name/"$username" --user "$apiuser:$apipass")
 #cleans up the name and adds a + between the white space so curl doesnt see it as a carriage return
 full_name=$(echo $response | /usr/bin/awk -F'<full_name>|</full_name>' '{print $2}'| sed 's/ /+/g')
 #cleans up the email and changes @ to %40
 email=$(echo $response | /usr/bin/awk -F'<email>|</email>' '{print $2}'| sed 's/@/%40/g')
 #cleans up the job title or as hicphat calls "position" and adds a + between the white space so curl doesnt see it as a carriage return
 position=$(echo $response | /usr/bin/awk -F'<position>|</position>' '{print $2}'| sed 's/ /+/g')
-
 #gets a list of all users in hipchat 
 hipchatuserlist=`curl -sS https://api.hipchat.com/v1/users/list?auth_token="$hipChatToken"`
 #looks for user
@@ -90,19 +90,14 @@ if [ "$hipchatuserexists" = "" ]; then
 		fi
 	sleep 1
 	done
-	hcp=`echo "$hipchatpass1" | sed -n 2p`
-	#count the length of said password
-	hcplength=`echo ${#hcp}`
-
 	
 	#push info to hicphat
-	hipchatresponse=`curl -d "email="$email"&name="$full_name"&title="$position"&password="$hcp"" https://api.hipchat.com/v1/users/create?auth_token="$hipChatToken"`
 
-	info=`"$CD" bubble --title "All done!" --text "Please login to hipchat, your username is: $username@pingidentity.com" --icon-file /Library/Application\ Support/JAMF/EndUserSupport/PI_SQLogo.icns`
+	hipchatresponse=`curl -d "email="$email"&name="$full_name"&title="$position"&password="$hcp"" https://api.hipchat.com/v1/users/create?auth_token="$hipChatToken"`
+	info=`"$CD" bubble  --timeout "5" --title "All done!" --text "Please login to hipchat, your username is: $username@pingidentity.com" --icon-file /Library/Application\ Support/JAMF/EndUserSupport/PI_SQLogo.icns`
 
 	exit 0
 else
 	echo "$username exists"
 	exit 0
 fi
-
